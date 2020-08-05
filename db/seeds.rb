410e5c73b618f4feaecbc6b51a0646ec91434433
Tomas Perez <tomasitopg6@gmail.com>
2020-08-05 16:07:26 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,3 @@ Student.create(name: "April", grade: "10th")
 Student.create(name: "Luke", grade: "9th")
 Student.create(name: "Devon", grade: "11th")
 Student.create(name: "Sarah", grade: "10th")
-
-namespace :db do
-
-  ...
-
-  desc 'seed the database with some dummy data'
-  task :seed do
-    require_relative './db/seeds.rb'
-  end
-end
